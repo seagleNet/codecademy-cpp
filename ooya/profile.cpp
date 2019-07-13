42c73cc685db621837532261a85a70f205df80a9
seagleNet <marc.sommerau@gmail.com>
2019-07-13 22:23:39 +0200

    im dumb

--- ooya/profile.cpp
+++ ooya/profile.cpp
@@ -23,10 +23,10 @@ string Profile::view_profile() {
   profile_info += "\nCountry: " + country;
   profile_info += "\nHobbies: ";
   for (size_t i = 0; i < hobbies.size(); i++) {
-    if (i > 0) {
-      profile_info += ", " + hobbies[i];
-    } else {
+    if (i == 0) {
       profile_info += hobbies[i];
+    } else {
+      profile_info += ", " + hobbies[i];
     }
   }
 
