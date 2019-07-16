3487e70bf90aee145419e4f065700c0d2dbf39d1
seagleNet <marc.sommerau@gmail.com>
2019-07-16 23:18:03 +0200

    add more and other hobbies

--- ooya/app.cpp
+++ ooya/app.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main() {
   Profile sam("Sam Drakkila", 30, "New York", "USA", "he/him");
-  sam.add_hobby("Gaming");
-  sam.add_hobby("Anime");
+  sam.add_hobby("listening to audiobooks and podcasts");
+  sam.add_hobby("playing rec sports like bowling and kickball");
+  sam.add_hobby("writing a speculative fiction novel");
+  sam.add_hobby("reading advice columns");
   cout << sam.view_profile() << "\n";
 }
