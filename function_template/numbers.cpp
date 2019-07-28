6594e1861f1306062a82d40c8c5b6f61e681fb02
seagleNet <marc.sommerau@gmail.com>
2019-07-28 23:29:07 +0200

    Added new course files

---
+++ function_template/numbers.cpp
@@ -0,0 +1,11 @@
+// Remove everything here:
+
+/* Overloaded functions
+int get_smallest(int num1, int num2) {
+  return num2 < num1 ? num2 : num1;
+}
+
+double get_smallest(double num1, double num2) {
+  return num2 < num1 ? num2 : num1;
+}
+*/
---
+++ function_template/numbers.hpp
@@ -0,0 +1,11 @@
+// Replace these declarations with a template
+
+template <typename T>
+T get_smallest(T num1, T num2) {
+  return num2 < num1 ? num2 : num1;
+}
+
+/* Overloaded functions:
+int get_smallest(int num1, int num2);
+double get_smallest(double num1, double num2);
+*/
